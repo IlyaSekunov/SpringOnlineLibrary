06eb1d947950226c77b8e2d14948b0deeca44590
Sekunov Ilya <sekunovila@gmail.com>
2024-04-05 12:07:44 +0300

    fix cycle dependencies

--- src/main/java/ru/ilya/spring_learning_library/SpringLearningLibraryApplication.java
+++ src/main/java/ru/ilya/spring_learning_library/SpringLearningLibraryApplication.java
@@ -2,10 +2,18 @@ package ru.ilya.spring_learning_library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringLearningLibraryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearningLibraryApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 }
--- src/main/java/ru/ilya/spring_learning_library/service/UserDetailsServiceImpl.java
+++ src/main/java/ru/ilya/spring_learning_library/service/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.ilya.spring_learning_library.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
